b2273475b27c956746652205c8f50210e8ad0b64
Shuvo-Roy <shuvoroysp@gmail.com>
2022-02-28 11:42:09 +0600

    add some java script

--- script.js
+++ script.js
@@ -14,3 +14,37 @@ const richestPeople = [
     'Michael Bloomberg',
     'larry Page'
 ];
+
+// store list items
+const listItems=[];
+
+
+let dragStartIndex;
+
+createList();
+
+//insert list item into dom
+function createList(){
+    [...richestPeople]
+    .map(a=>({value:a , sort:Math.random()}))
+    .sort((a,b) => a.sort - b.sort)
+    .map(a =>a.value)
+    .forEach((person, index)=>{
+        console.log(person);
+        const listItem = document.createElement('li');
+
+        listItem.setAttribute('data-index',index);
+
+        listItem.innerHTML = `
+        <span class="number">${index +1}</span>
+        <div class ="draggable" draggable ="true">
+            <p class="person-name">${person}</p>
+            <i class="fas fa-grip-lines"></li>
+        </div>
+        `;
+
+        listItems.push(listItem);
+
+        draggable_list.appendChild(listItem);
+    });
+}
